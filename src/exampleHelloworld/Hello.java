9039300a4132d6045e38fda6c86fd8708323f0b4
moolamdeepthi <deepthi.moolam@blr.velankani.com>
2016-03-31 10:19:31 +0530

    first commit

---
+++ src/exampleHelloworld/Hello.java
@@ -0,0 +1,11 @@
+package exampleHelloworld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println ("Hello world");
+System.out.println("")
+	}
+
+}
